a57bd65fc9dbb4c6687c0d5bdb3f7bc8e5a75a49
al34034 <alex@nitters.eu>
2018-02-23 10:43:59 +0100

    Using maps, fixed #1

--- Qwant_search@alex.nitters.eu/extension.js
+++ Qwant_search@alex.nitters.eu/extension.js
@@ -1,3 +1,4 @@
+
 // create atom project opener
 // view log: journalctl /usr/bin/gnome-session -f -o cat
 
@@ -23,7 +24,7 @@ let baseGIcon;
 let hoverGIcon;
 let buttonIcon;
 
-var debug = false;
+var debug = true;
 
 function logDebug() {
   if (debug) {
@@ -45,26 +46,26 @@ function makeLaunchContext(params) {
     workspace: -1,
     timestamp: global.display.get_current_time_roundtrip()
   });
-  
+
   let launchContext = global.create_app_launch_context(params.timestamp, params.workspace);
-  
+
   return launchContext;
 }
 
 function countProperties(obj) {
   var count = 0;
-  
+
   for(var prop in obj) {
     if(obj.hasOwnProperty(prop))
     ++count;
   }
-  
+
   return count;
 }
 
 const QwantSearchProvider = new Lang.Class({
   Name: 'QwantSearchProvider',
-  
+
   _init : function(title, categoryType) {
     this._categoryType = categoryType;
     this._title = title;
@@ -76,23 +77,23 @@ const QwantSearchProvider = new Lang.Class({
     };
     this.qwantResults = new Map();
   },
-  
+
   _getResultSet: function(terms) {
     logDebug("getResultSet");
     var resultIds = Array.from(this.qwantResults.keys())
-    
-    
+
+
     logDebug("found " + resultIds.length + " results" );
     return resultIds;
   },
-  
+
   getResultMetas: function(resultIds, callback) {
     logDebug("result metas for name: "+resultIds.join(" "));
     let metas = resultIds.map(id => this.getResultMeta(id));
     logDebug("metas: " + metas.join(" "));
     callback(metas);
   },
-  
+
   getResultMeta: function(resultId) {
     let result = this.qwantResults.get(resultId);
     let name = result.name;
@@ -106,7 +107,7 @@ const QwantSearchProvider = new Lang.Class({
       'createIcon' : function(size) {}
     }
   },
-  
+
   processTerms: function(terms, callback, cancellable) {
     this.qwantResults.clear();
     var joined = terms.join(" ");
@@ -115,9 +116,9 @@ const QwantSearchProvider = new Lang.Class({
     logDebug("Search with: " + joined);
     this.getSuggestions(terms, callback)
   },
-  
+
   getSuggestions: function(terms, callback) {
-    
+
     var suggestions = {};
     let request = Soup.form_request_new_from_hash(
       'GET',
@@ -129,53 +130,51 @@ const QwantSearchProvider = new Lang.Class({
     _httpSession.queue_message(request, Lang.bind(this,
       function (_httpSession, response) {
         if (response.status_code === 200) {
-          
+
           let jsonItems = (JSON.parse(response.response_body.data).data.items);
           let jsonSpecial = (JSON.parse(response.response_body.data).data.special);
           logDebug("bodydata", response.response_body.data);
-          var suggestions = {0: {}};
-          
-          for (i = 0; i < countProperties(jsonSpecial); i++) {
-            logDebug("Adding special: " + jsonSpecial[i].name)
-            if (jsonSpecial[i].name == terms.join(" ")) {continue};
-            suggestions[i] = {type: "special", name: jsonSpecial[i].name, description: jsonSpecial[i].description, url: searchUrl + encodeURIComponent(jsonSpecial[i].name)}
-          }
-          
-          for (i + 1; i < countProperties(jsonItems); i++) {
-            logDebug("Adding suggestion: " + jsonItems[i].value)
-            if (jsonItems[i].value == terms.join(" ")) {continue};
-            if (jsonItems[i].value.startsWith("&")) {
-              suggestions[i] = {type: "special", name: jsonItems[i].value, description: jsonItems[i].site_name, url: searchUrl + encodeURIComponent(jsonItems[i].value)}
-            } else {
-              suggestions[i] = {type: "suggestion", name: jsonItems[i].value, url: searchUrl + encodeURIComponent(jsonItems[i].value)}
+          var parsedItems = jsonItems
+          .filter(suggestion => suggestion.value != terms.join(" "))
+          .map(suggestion => {
+            if (suggestion.value.startsWith("&")) {
+              return {type: "special", name: suggestion.value, description: suggestion.site_name, url: searchUrl + encodeURIComponent(suggestion.value)};
             }
-          }
-          
+            else {
+              return {type: "suggestion", name:suggestion.value, url: searchUrl + encodeURIComponent(suggestion.value)};
+            }
+          });
+          var parsedSpecial = jsonSpecial
+          .filter(suggestion => suggestion.value != terms.join(" "))
+          .map(suggestion => (
+            {type: "special", name: suggestion.name, description: suggestion.description, url: searchUrl + encodeURIComponent(suggestion.name)}
+          ));
+          suggestions = parsedSpecial.concat(parsedItems);
         }
         else {
           suggestions[0] = {type: "result", name: "Request failed", description: "Please check your Internet or try again later", url: ""}
         }
         this.displaySuggestions(suggestions, callback, terms);
-        
+
       })
     );
-    
-    
-    
+
+
+
     /********************TODO: Get results from Qwant********************/
-    
+
   },
-  
+
   displaySuggestions: function(suggestions, callback, terms) {
     for (var i = 0; i < countProperties(suggestions); i++) {
-      if (suggestions[i].type == "suggestion") {this.qwantResults.set(suggestions[i].url, makeResult(" ", suggestions[i].name, function () {}, suggestions[i].url)); }
+      if (suggestions[i].type == "suggestion") {this.qwantResults.set(suggestions[i].url, makeResult(" ", suggestions[i].name, function () {}, suggestions[i].url)); };
       if (suggestions[i].type == "special") {this.qwantResults.set(suggestions[i].url, makeResult(suggestions[i].name , suggestions[i].description, function () {}, suggestions[i].url)); }
       if (suggestions[i].type == "result") {this.qwantResults.set(suggestions[i].url, makeResult(suggestions[i].name , suggestions[i].description, function () {}, suggestions[i].url)); }
-      
+
     }
     callback(this._getResultSet(terms));
   },
-  
+
   activateResult: function(resultId, terms) {
     var result = this.qwantResults[resultId];
     logDebug("activateResult: " + resultId);
@@ -186,7 +185,7 @@ const QwantSearchProvider = new Lang.Class({
       makeLaunchContext({})
     );
   },
-  
+
   launchSearch: function(result) {
     logDebug("launchSearch: " + result.name);
     Gio.app_info_launch_default_for_uri(
@@ -194,25 +193,25 @@ const QwantSearchProvider = new Lang.Class({
       makeLaunchContext({})
     );
   },
-  
+
   getInitialResultSet: function(terms, callback, cancellable) {
     logDebug("SuggestionId: " + this.suggestionId);
     logDebug("getInitialResultSet: " + terms.join(" "));
     this.processTerms(terms, callback, cancellable);
   },
-  
+
   filterResults: function(results, maxResults) {
     logDebug("filterResults", results, maxResults);
     return results.slice(0, maxResults);
     //return results;
   },
-  
+
   getSubsearchResultSet: function(previousResults, terms, callback, cancellable) {
     logDebug("getSubSearchResultSet: " + terms.join(" "));
     this.processTerms(terms, callback, cancellable, );
   },
-  
-  
+
+
 });
 
 function _openQwant() {
@@ -236,7 +235,7 @@ function init(extensionMeta) {
     'gicon': Gio.icon_new_for_string(Me.path + "/icons/system_status_icon.png"),
     'style_class': 'system-status-icon'
   });
-  
+
   button.set_child(buttonIcon);
   button.connect('button-press-event', Lang.bind(this, _openQwant));
   button.connect('enter-event', function() {
