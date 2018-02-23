df3906e793b84aac790c90c2920a721853c270cf
al34034 <alex@nitters.eu>
2018-02-23 16:51:51 +0100

    Removed unecessary installer

--- qwant-gnome-shell-integration.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/al34034/Qwant-Gnome-shell-integration/ /tmp/qwant_gnome/
-cp -R /tmp/qwant_gnome/Qwant_search@alex.nitters.eu/ ~/.local/share/gnome-shell/extensions/Qwant_search@alex.nitters.eu/
-rm -rf /tmp/qwant_gnome/
-gnome-shell-extension-tool -e Qwant_search@alex.nitters.eu
